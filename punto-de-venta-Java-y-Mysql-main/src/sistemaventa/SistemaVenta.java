41c2c76640bf8066049b8f601b4349f0cb53d72e
RobertoAlanoca <roberto.alanoca@ucb.edu.bo>
2025-05-07 00:46:51 -0400

    Arreglo del error voluntario

--- punto-de-venta-Java-y-Mysql-main/src/sistemaventa/SistemaVenta.java
+++ punto-de-venta-Java-y-Mysql-main/src/sistemaventa/SistemaVenta.java
@@ -11,7 +11,7 @@ public class SistemaVenta {
     /**
      * @param args the command line arguments
      */
-    //public static void main(String[] args) {
+    public static void main(String[] args) {
         // TODO code application logic here
         Login lg = new Login();
         lg.setVisible(true);
